a19d74092c8c162ca7f552f4350ac53b83bed41d
Shogo Ikari <anchor200@ShogonoMacBook-Pro.local>
2019-05-28 16:39:39 +0900

    wallstop2

--- scripts/wall_stop.py
+++ scripts/wall_stop.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-import rospy.copy
+import rospy, copy
 from geometry_msgs.msg import Twist
 from std_srvs.srv import Trigger, TriggerResponse
 from raspicat_basic.msg import LightSensorValues
 
 
-class WallStop():
+class WallStop:
     def __init__(self):
         self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 
@@ -25,7 +25,7 @@ class WallStop():
             rate.sleep()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     rospy.init_node('wall_stop')
     rospy.wait_for_service('/motor_on')
     rospy.wait_for_service('/motor_off')
